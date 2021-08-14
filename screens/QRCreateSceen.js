27b4cfe9c416567c2856cc7395b0a1ce6ab95fe8
Austin <madeinsg3@gmail.com>
2021-08-14 17:09:55 +1000

    yarn.lock LF replaced by CRLF

--- screens/QRCreateSceen.js
+++ screens/QRCreateSceen.js
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
-import { Dimensions, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Image, View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import TabBar from './assets/TabBar';
 import { GLOBAL } from './assets/GLOBAL';
 
 const QRCreateScreen = () => {
   const [TIValue, setTIValue] = useState('')
-  console.log(TIValue)
+  const [QRStatus, setQRStatus] = useState(false)
+  const [QRValue, setQRValue] = useState('')
+  const generateFunction = (textValue) => {
+    if (textValue == ''){
+      console.log('empty')
+    }else{
+      setQRStatus(true)
+      setQRValue(TIValue)
+    }
+  }
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
         <View style={styles.topContainer}>
+          {
+            !QRStatus ?
+            (
+              <Image
+                style={styles.questionMarkQR}
+                source={require('./assets/questionMark.jpg')}
+              />
+            ):
+            (
+              <QRCode 
+              value={QRValue}          
+              size={250}
+            />
+            )
+          }
 
         </View>
         <View style={styles.bottomContainer}>
@@ -19,11 +43,11 @@ const QRCreateScreen = () => {
               <Text style={styles.inputLabelText}>Value : </Text>
             </View>
             <View style={styles.inputTIContainer}>
-              <TextInput style={styles.inputTI} multiline={true}/>
+              <TextInput style={styles.inputTI} multiline={true} onChangeText={(text) => setTIValue(text)}/>
             </View>
           </View>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.buttonTO} onChange={(text) => {setTIValue(text); console.log(text)}}>
+            <TouchableOpacity style={styles.buttonTO} onPress={() => generateFunction(TIValue)}>
               <Text style={styles.buttonText}>Generate</Text>
             </TouchableOpacity>
           </View>
@@ -49,6 +73,8 @@ const styles = StyleSheet.create({
   topContainer: {
     flex: 5,
     overflow: 'hidden',
+    alignItems: 'center', 
+    justifyContent: 'center', 
   },
   bottomContainer: {
     flex: 2,
@@ -88,5 +114,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     paddingVertical: 10,
     borderRadius: 5
+  },
+  questionMarkQR: {
+    height: 250, 
+    width: 250,
   },
 })
