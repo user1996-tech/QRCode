34d26fc544b9ddc98f0fbefe6f5ffa69b136355e
Austin Loh <madeinsg3@gmail.com>
2021-08-17 21:33:11 +1000

    NFC tried

--- screens/NFCScreen.js
+++ screens/NFCScreen.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Dimensions, View, Text, StyleSheet } from 'react-native';
 import TabBar from './assets/TabBar';
 import { GLOBAL } from './assets/GLOBAL';
+import NfcManager, {NfcEvents} from 'react-native-nfc-manager';
 
 const NFCScreen = () => {
+  const initNfc = async () => {
+    await NfcManager.start()
+  }
+
+  const readNdef = () => {
+    const cleanUp = () => {
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, null);
+      NfcManager.setEventListener(NfcEvents.SessionClosed, null)
+    }
+    
+    return new Promise((resolve) => {
+      let tageFound = null;
+      NfcManager.setEventListener(NfcEvents.DiscoverTag, (tag) => {
+        tagFound = tag;
+        resolve(tagFound)
+        NfcManager.setAlertMessage('NDEF tag Found')
+        NfcManager.unregisterTagEvent().catch(() => 0)
+      })
+
+      NfcManager.setEventListener(NfcEvents.SessionClosed, () => {
+        cleanUp()
+        if(!tagFound){
+          resolve()
+        }
+      })
+      NfcManager.registerTagEvent()
+    })
+  }
+  
   return (
     <View style={styles.container}>
       <View style={styles.contentContainer}>
